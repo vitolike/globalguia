820a55a76fa9d788b1da0660464df4afe53690b9
vitolike <victorcostaoliveira@gmail.com>
2020-02-17 13:24:52 -0800

    Config e Controller Admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,16 +5,35 @@ class Admin extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->entrar();
     }
-    
+	
+	//pagina de login vai aqui
     public function entrar()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('paginas/login');
     }
-    
-    public function autenticar()
+	
+	public function autenticar()
 	{
-		$this->load->view('welcome_message');
+		$email = $this->input->post('email');
+		$senha = md5($this->input->post('senha'));
+		
+		$this->db->where('email',$email);
+		$this->db->where('senha',$senha);
+		$data['admins'] = $this->db->get('admins')->result();
+		
+		if(count($data['admins']) == 1){
+			$dados['nome'] = $data['admins'][0]->nome;
+			$dados['id'] = $data['admins'][0]->id;
+			$dados['logado'] = true;
+			$this->session->set_userdata($dados);
+			
+			echo 'SUCESSO MEU QUERIDO';
+		}
+		else{
+			echo 'ERRO';	
+			}
+		
 	}
 }
